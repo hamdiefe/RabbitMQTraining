28b2be1780b6697df8c22804c75c5988d400a51d
hamdiefe <hamdi.efe@outlook.com>
2024-02-23 22:05:44 +0300

    Add project files.

---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,27 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMQ.Publisher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://xnyudbqc:P7iGw2JCjmXYBhfU7co79ZpptepxQylq@beaver.rmq.cloudamqp.com/xnyudbqc");
+
+
+            using (var connection = factory.CreateConnection())
+            {
+                var channel = connection.CreateModel();
+                channel.QueueDeclare("Hello Queue", true, false, false);
+                var message = "Hello World";
+                var messageBody = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(string.Empty, "Hello Queue", null, messageBody);
+                Console.WriteLine("Mesaj Gönderildi.");
+            }
+
+        }
+    }
+}
